a98c8e28cabb58da9464bccf9a60ea0c17b0a77d
bbearcookie <aodem@naver.com>
2023-10-24 00:33:08 +0900

    feat: useModifyPost 예시 훅 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useQuery, useSuspenseQuery, useMutation } from '@tanstack/react-query';
+import useModifyPost from './api/mutations/useModifyPost';
 import postAPI from './api/functions/postAPI';
 import queryKey from './api/queryKey';
 import './App.css';
@@ -8,15 +9,21 @@ function App() {
   const { data: postDetail } = useQuery({ ...queryKey.posts.detail('5') });
   const { data: commentDetail } = useSuspenseQuery({ ...queryKey.comments.detail('3') });
 
-  const { mutate } = useMutation({ mutationFn: postAPI.addPost });
+  const { mutate: addPost } = useMutation({ mutationFn: postAPI.addPost });
+  const { mutate: modifyPost } = useModifyPost();
 
   const handleAddPost = () => {
-    mutate({ id: 1, title: 'title', body: 'body', userId: 1 });
+    addPost({ id: 1, title: 'title', body: 'body', userId: 1 });
+  };
+
+  const handleModifyPost = () => {
+    modifyPost({ id: 1, title: 'title', body: 'body', userId: 1 });
   };
 
   return (
     <div>
       <button onClick={handleAddPost}>게시글 추가 요청</button>
+      <button onClick={handleModifyPost}>게시글 수정 요청</button>
     </div>
   );
 }
---
+++ src/api/mutations/useModifyPost.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Post } from '@/types';
+import postAPI from '../functions/postAPI';
+import queryKeys from '../queryKey';
+
+const useModifyPost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: postAPI.modifyPost,
+    onMutate: async (post) => {
+      const { queryKey } = queryKeys.posts.detail(`${post.id}`);
+      await queryClient.cancelQueries(queryKeys.posts.detail(`${post.id}`));
+
+      const previousPost = queryClient.getQueryData<Post>(queryKey);
+
+      queryClient.setQueryData<Post>(queryKey, post);
+
+      return { previousPost };
+    },
+    onError: (err, post, context) => {
+      queryClient.setQueryData<Post>(queryKeys.posts.detail(`${post.id}`).queryKey, context?.previousPost);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(queryKeys.posts.all);
+    },
+  });
+};
+
+export default useModifyPost;
